e57b5c85afc89f31f1c46d91641bb999277a987f
zbyju <zbynek.jurica@gmail.com>
2024-05-28 21:32:28 +0200

    Solve need for speed

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,39 @@
+package speed
+
+import "math"
+
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{battery: 100, batteryDrain: batteryDrain, speed: speed, distance: 0}
+}
+
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery < car.batteryDrain {
+		return car
+	}
+	return Car{battery: car.battery - car.batteryDrain, batteryDrain: car.batteryDrain, speed: car.speed, distance: car.distance + car.speed}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	wouldDrain := int(math.Ceil(float64(track.distance) / float64(car.speed)))
+	return wouldDrain*car.batteryDrain <= car.battery
+}
