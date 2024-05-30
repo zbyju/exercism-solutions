5d41280028fa480b1e8f21a5884d8f81a74930e6
zbyju <zbynek.jurica@gmail.com>
2024-05-30 20:46:00 +0200

    Solve interest is interesting

---
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,35 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance >= 5000:
+		return 2.475
+	case balance >= 1000:
+		return 1.621
+	default:
+		return 0.5
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * float64(InterestRate(balance)) / 100
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	i := 0
+	for balance < targetBalance {
+		i++
+		balance = AnnualBalanceUpdate(balance)
+	}
+	return i
+}
